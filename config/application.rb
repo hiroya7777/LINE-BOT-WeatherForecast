5945cf1fcf1aeeace2f8985196bd41fe2a406548
kawaguchi <kawaguchihiroya1117@outlook.jp>
2019-01-10 21:42:21 +0900

    otenki-bot

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module Otenki
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.time_zone = 'Okayama'
+    config.time_zone = 'Tokyo'
     config.i18n.default_locale = :ja
   end
 end
